058a87af258ba90492cbf4a85ed9a421b86a61bd
BritvA <apan1998@yandex.ru>
2022-09-06 12:21:14 +0300

    Добавление паттерна MVVM

---
+++ Infrastructure/Commands/WindowMouseMoveCommand.cs
@@ -0,0 +1,16 @@
+﻿using Sequence.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequence.Infrastructure.Commands
+{
+    internal class WindowMouseMoveCommand:Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Sequence.Infrastructure.Commands;
+using Sequence.ViewModels.Base;
+
+namespace Sequence.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        private string _Title = "Sequence";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
