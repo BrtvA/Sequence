70d0012fd87c246a9ae29864db4fbd726b2635e9
BritvA <apan1998@yandex.ru>
2023-02-13 15:39:06 +0300

    #2. Изменена ViewModel, добавлена валидация

---
+++ src/Sequence/Models/InputModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Sequence.Models
+{
+    class InputModel
+    {
+        [JsonIgnore]
+        public string Path { get; set; }
+        public int BitADC { get; set; }
+        public int NumberLSB { get; set; }
+        public int TimeShift { get; set; }
+        public bool IsCustomTimeSeries { get; set; }
+        public int TimeSeries { get; set; }
+
+
+        public InputModel(string path, string bitADC, string numberLSB, string timeShift, bool isCustomTimeSeries, string timeSeries)
+        {
+            Path = path;
+
+            int.TryParse(bitADC, out int adc);
+            BitADC = adc;
+
+            int.TryParse(numberLSB, out int lsb);
+            NumberLSB = lsb;
+
+            int.TryParse(timeShift, out int shift);
+            TimeShift = shift;
+
+            IsCustomTimeSeries = isCustomTimeSeries;
+
+            int.TryParse(timeSeries, out int series);
+            TimeSeries = series;
+        }
+
+        public InputModel()
+        {
+
+        }
+    }
+}
